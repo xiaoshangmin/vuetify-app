9ab78a8282c59ff72213d5e6a4364980c6742bd9
shangminxiao <shangminxiao@gmail.com>
2022-07-09 11:08:07 +0800

    u

--- src/main.js
+++ src/main.js
@@ -10,8 +10,6 @@ import * as socketApi from './utils/socket'
 
 axios.defaults.timeout = 10000;
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
-console.log(123,import.meta.env.VITE_API_BASE_URL)
-
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL
 
 loadFonts()
