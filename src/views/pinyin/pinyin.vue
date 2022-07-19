fca20e09f4e52abaece0aa9b98493291759c6906
xiaoshangmin <Xsmxsm0220@gmail.com>
2022-07-19 18:23:13 +0800

    u

--- src/views/pinyin/pinyin.vue
+++ src/views/pinyin/pinyin.vue
@@ -3,14 +3,17 @@
     <v-row justify="center">
       <v-card :width="width">
         <v-card-text>
-          <v-text-field label="请输入中文" clearable
-                        v-model="chinese" @input="onChange"
-          ></v-text-field>
-          <v-switch v-model="toneType" label="不带声调" color="red" value="true">
+          <v-text-field label="请输入中文" clearable v-model="chinese" @input="onChange"></v-text-field>
+          <v-switch v-model="toneType" label="不带声调" color="red" value="true" v-on:change="switchMe">
           </v-switch>
-          <p class="font-weight-medium text-h5">
-            {{ messages }}
-          </p>
+          <div class="text-caption pa-2 bg-grey-lighten-3">
+            <div class="text-grey"> 拼音</div>
+            <div class="font-weight-medium text-h5">{{ messages }}</div>
+          </div>
+          <div class="text-caption pa-2 bg-grey-lighten-4">
+            <div class="text-grey"> 繁体</div>
+            <div class="font-weight-medium text-h5">{{ trad }}</div>
+          </div>
         </v-card-text>
       </v-card>
     </v-row>
@@ -18,12 +21,14 @@
 </template>
 
 <script>
-import {pinyin} from 'pinyin-pro';
+import { pinyin } from 'pinyin-pro';
+import cnchar from 'cnchar-all';
 
 export default {
   data: () => ({
     chinese: '',
     messages: '',
+    trad: '',
     toneType: false,
   }),
   computed: {
@@ -43,17 +48,24 @@ export default {
     },
   },
   created() {
-
   },
   methods: {
+    switchMe() {
+      this.onChange()
+    },
     onChange() {
       if (this.chinese) {
-        let options = {v: true, toneType: this.toneType, removeNonZh: true}
+        let options = { v: true, toneType: this.toneType, removeNonZh: true }
         if (this.toneType) {
           options.toneType = 'none'
         }
-        console.log(options)
         this.messages = pinyin(this.chinese, options)
+        if (cnchar.isCnChar(this.chinese)) {
+          this.trad = cnchar.convert.simpleToTrad(this.chinese)
+        }
+      }else{
+        this.messages = '';
+        this.trad = ''
       }
     }
   }
@@ -61,5 +73,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
