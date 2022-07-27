d30959cdc04a0022e261e74cbc3b0326042497dc
xiaoshangmin <Xsmxsm0220@gmail.com>
2022-07-27 11:39:13 +0800

    u

--- src/views/timestamp/timestamp.vue
+++ src/views/timestamp/timestamp.vue
@@ -1,25 +1,37 @@
 <template>
   <v-container fluid>
-    <v-row justify="center" align="center">
-      <v-col cols="3">
-        <v-text-field :label="timestamp">
+    <v-row justify="center" align="center" class="mt-3">
+      <v-col md="5" sm="12">
+        <v-text-field :placeholder="nowTimestamp" hide-details v-model="timestamp" variant="outlined" @input="toDateTimeMethod">
           <template v-slot:prepend>
-            <span>时间戳：</span>
+            <span>时间戳(秒)：</span>
           </template>
           <template v-slot:append>
-            <span>{{ d }}</span>
+            <span>{{ toDateTime }}</span>
           </template>
         </v-text-field>
       </v-col>
     </v-row>
     <v-row justify="center" align="center">
-      <v-col cols="3">
-        <v-text-field :label="datetime">
+      <v-col md="5" sm="12">
+        <v-text-field :placeholder="nowDatetime" hide-details v-model="datetime" variant="outlined" @input="toTimeStampMethod">
           <template v-slot:prepend>
             <span>北京时间：</span>
           </template>
           <template v-slot:append>
-            <span>{{ d }}</span>
+            <span>{{ toTimeStamp }}</span>
+          </template>
+        </v-text-field>
+      </v-col>
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-col md="5" sm="12">
+        <v-text-field v-model="nowD" readonly variant="outlined">
+          <template v-slot:prepend>
+            <span>当前时间：</span>
+          </template>
+          <template v-slot:append>
+            <span>{{ nowT }}</span>
           </template>
         </v-text-field>
       </v-col>
@@ -30,14 +42,33 @@
 <script>
 export default {
   data: () => ({
-    timestamp: (new Date()).getTime(),
-    datetime: (new Date()).getTime(),
-    d: '2022'
+    nowTimestamp: parseInt((new Date()).getTime() / 1000).toString(),
+    nowDatetime: (new Date()).toLocaleString(),
+    toDateTime: '',
+    toTimeStamp: '',
+    timestamp: '',
+    datetime: '',
+    nowT: '',
+    nowD: '',
   }),
-
+  computed: {},
   created() {
+    setInterval(() => {
+      this.nowT = parseInt(Date.now() / 1000)
+      this.nowD = (new Date()).toLocaleString()
+    }, 1000)
   },
-  methods: {}
+  methods: {
+    toDateTimeMethod() {
+      let date = new Date(this.timestamp * 1000)
+      this.toDateTime = date.toLocaleString()
+    },
+    toTimeStampMethod() {
+      let date = (new Date(this.datetime)).getTime()
+      date = parseInt((date / 1000))
+      this.toTimeStamp = date
+    }
+  }
 }
 </script>
 
