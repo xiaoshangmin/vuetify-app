b4015b5f55a30759abd884e91d29bf07a07272dc
xiaoshangmin <Xsmxsm0220@gmail.com>
2022-08-23 17:44:53 +0800

    update

--- src/views/md5/md5.vue
+++ src/views/md5/md5.vue
@@ -4,32 +4,18 @@
       <v-col md="12" sm="12" lg="10">
         <v-row justify="center">
           <v-col md="12" sm="12" lg="12">
-            <v-textarea variant="outlined" v-model="content" clearable label="请输入需要加密的内容" auto-grow
-                        hide-details rows="2" @input="update">
+            <v-textarea variant="outlined" v-model="content" clearable label="请输入需要加密的内容" auto-grow hide-details
+              rows="2" @input="update">
             </v-textarea>
           </v-col>
         </v-row>
-        <v-col md="12" sm="12" lg="10">
-          <v-radio-group
-              v-model="encodeRadio"
-              inline
-          >
-            <v-radio
-                label="MD5"
-                color="red"
-                value="md5"
-            ></v-radio>
-            <v-radio
-                label="base64"
-                color="red"
-                value="base64"
-            ></v-radio>
-          </v-radio-group>
-        </v-col>
-        <v-card max-width="1400" v-if="message" class="mt-4">
+        <v-card max-width="1500" class="mt-4">
           <v-card-subtitle>结果</v-card-subtitle>
           <v-card-text>
-            {{ message }}
+            <div class="text-subtitle-1">MD5: <span class="text-h6">{{ md5Text }} </span></div>
+            <div class="text-subtitle-1">BASE64: <span class="text-h6">{{ base64Text }} </span></div>
+            <div class="text-subtitle-1">url编码: <span class="text-h6">{{ urlEncodeText }} </span></div>
+            <div class="text-subtitle-1">url解码:<span class="text-h6">{{ urlDecodeText }} </span></div>
           </v-card-text>
         </v-card>
       </v-col>
@@ -39,24 +25,24 @@
 
 <script>
 import md5 from "js-md5";
-import {Base64} from 'js-base64'
+import { Base64 } from 'js-base64'
 
 export default {
   data: () => ({
-    encodeRadio: 'md5',
     content: '',
-    message: ''
+    md5Text: '',
+    base64Text: '',
+    urlDecodeText: '',
+    urlEncodeText: '',
   }),
   methods: {
     update() {
       if (this.content) {
-        let res = ''
-        if (this.encodeRadio == 'md5') {
-          res = md5(this.content)
-        } else if (this.encodeRadio == 'base64') {
-          res = Base64.encode(this.content)
-        }
-        this.message = `${this.encodeRadio}(${this.content}) = ${res}`
+        this.md5Text = md5(this.content)
+        this.base64Text = Base64.encode(this.content)
+        this.urlEncodeText = encodeURIComponent(this.content).replace(/!/g, '%21').replace(/'/g, '%27').replace(/\(/g, '%28').
+          replace(/\)/g, '%29').replace(/\*/g, '%2A').replace(/%20/g, '+');
+        this.urlDecodeText = decodeURIComponent(this.content)
       }
     }
   },
